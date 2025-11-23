913e02db35c52d66183716f859d851ab77ba8f76
crisirius <80912325+crisirius@users.noreply.github.com>
2025-11-23 16:45:36 +0100

    Update dbus-shelly-em-smartmeter.py
    Position in victron noch aus config 0 1 und 2 möglich

--- dbus-shelly-em-smartmeter.py
+++ dbus-shelly-em-smartmeter.py
@@ -23,34 +23,35 @@ class DbusShellyemService:
     config = self._getConfig()
     deviceinstance = int(config['DEFAULT']['Deviceinstance'])
     customname = config['DEFAULT']['CustomName']
-    self.channel = int(config['DEFAULT'].get('Channel', 0))      # Kanal Index (0 oder 1)
-    self.role = config['DEFAULT'].get('VictronRole', 'grid')     # Victron Role (pvinverter, grid, etc.)
-
+    self.channel = int(config['DEFAULT'].get('Channel', 0))           # Kanal Index (0 oder 1)
+    self.role = config['DEFAULT'].get('VictronRole', 'grid')          # Victron Role (pvinverter oder grid)
+    self.position = int(config['DEFAULT'].get('AcPosition', 0))       # AcPosition (0=AC input 1; 1=AC output; 2=AC input 2)
+    
     self._dbusservice = VeDbusService("{}.http_{:02d}".format(servicename, deviceinstance))
     self._paths = paths
-
+    
     logging.debug("%s /DeviceInstance = %d" % (servicename, deviceinstance))
-
+    
     # Create the management objects, as specified in the ccgx dbus-api document
     self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
     self._dbusservice.add_path('/Mgmt/ProcessVersion', 'Unknown version, running on Python ' + platform.python_version())
     self._dbusservice.add_path('/Mgmt/Connection', connection)
-
+    
     # Create the mandatory objects
     self._dbusservice.add_path('/DeviceInstance', deviceinstance)
     self._dbusservice.add_path('/ProductId', 45069)  # Beispiel ET340 Energy Meter
     self._dbusservice.add_path('/DeviceType', 345)   # Beispiel ET340 Energy Meter
     self._dbusservice.add_path('/ProductName', productname)
-    self._dbusservice.add_path('/CustomName', customname)
-    self._dbusservice.add_path('/Latency', None)
+    self._dbusservice.add_path('/CustomName', customname)    
+    self._dbusservice.add_path('/Latency', None)    
     self._dbusservice.add_path('/FirmwareVersion', 0.1)
     self._dbusservice.add_path('/HardwareVersion', 0)
     self._dbusservice.add_path('/Connected', 1)
-    self._dbusservice.add_path('/Role', self.role)    # Victron Role aus Config
-    self._dbusservice.add_path('/Position', 0)
+    self._dbusservice.add_path('/Role', self.role)          # Victron Role aus Config
+    self._dbusservice.add_path('/Position', self.position)  # AcPosition aus Config
     self._dbusservice.add_path('/Serial', self._getShellySerial())
     self._dbusservice.add_path('/UpdateIndex', 0)
-
+    
     # add path values to dbus
     for path, settings in self._paths.items():
       self._dbusservice.add_path(
@@ -60,39 +61,39 @@ class DbusShellyemService:
     self._lastUpdate = 0
 
     # add _update function 'timer'
-    gobject.timeout_add(250, self._update)  # Pause 250ms bevor nächster Request
-
-    # add _signOfLife 'timer' to get feedback in log every 5 minutes
+    gobject.timeout_add(250, self._update)  # Pause 250ms bevor nächste Anfrage
+    
+    # add _signOfLife 'timer' für Log-Feedback alle 5 Minuten
     gobject.timeout_add(self._getSignOfLifeInterval()*60*1000, self._signOfLife)
-
+ 
   def _getShellySerial(self):
-    meter_data = self._getShellyData()
+    meter_data = self._getShellyData()  
     if not meter_data['mac']:
         raise ValueError("Response does not contain 'mac' attribute")
     return meter_data['mac']
-
+ 
   def _getConfig(self):
     config = configparser.ConfigParser()
     config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
     return config
-
+ 
   def _getSignOfLifeInterval(self):
     config = self._getConfig()
     value = config['DEFAULT']['SignOfLifeLog']
-    if not value:
+    if not value: 
         value = 0
     return int(value)
-
+  
   def _getShellyStatusUrl(self):
     config = self._getConfig()
     accessType = config['DEFAULT']['AccessType']
-    if accessType == 'OnPremise':
+    if accessType == 'OnPremise': 
         URL = "http://%s:%s@%s/status" % (config['ONPREMISE']['Username'], config['ONPREMISE']['Password'], config['ONPREMISE']['Host'])
         URL = URL.replace(":@", "")
     else:
         raise ValueError("AccessType %s is not supported" % (config['DEFAULT']['AccessType']))
     return URL
-
+ 
   def _getShellyData(self):
     URL = self._getShellyStatusUrl()
     meter_r = requests.get(url=URL)
@@ -102,14 +103,14 @@ class DbusShellyemService:
     if not meter_data:
         raise ValueError("Converting response to JSON failed")
     return meter_data
-
+ 
   def _signOfLife(self):
     logging.info("--- Start: sign of life ---")
     logging.info("Last _update() call: %s" % (self._lastUpdate))
     logging.info("Last '/Ac/Power': %s" % (self._dbusservice['/Ac/Power']))
     logging.info("--- End: sign of life ---")
     return True
-
+ 
   def _update(self):
     try:
       meter_data = self._getShellyData()
@@ -160,13 +161,18 @@ def main():
       from dbus.mainloop.glib import DBusGMainLoop
       DBusGMainLoop(set_as_default=True)
 
+      config = configparser.ConfigParser()
+      config.read("%s/config.ini" % (os.path.dirname(os.path.realpath(__file__))))
+      victron_role = config['DEFAULT'].get('VictronRole', 'pvinverter')
+      servicename = f"com.victronenergy.{victron_role}"
+
       _kwh = lambda p, v: (str(round(v, 2)) + 'KWh')
       _a = lambda p, v: (str(round(v, 1)) + 'A')
       _w = lambda p, v: (str(round(v, 1)) + 'W')
       _v = lambda p, v: (str(round(v, 1)) + 'V')
 
       pvac_output = DbusShellyemService(
-        servicename='com.victronenergy.pvinverter',
+        servicename=servicename,
         paths={
           '/Ac/Energy/Forward': {'initial': 0, 'textformat': _kwh},
           '/Ac/Energy/Reverse': {'initial': 0, 'textformat': _kwh},
